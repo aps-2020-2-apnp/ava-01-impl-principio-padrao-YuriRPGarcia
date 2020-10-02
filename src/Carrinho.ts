c9a02d56f8434a66d1dcc09ae76919ad6e42d822
Yuri Garcia <yurirpgarcia@gmail.com>
2020-10-02 20:39:32 -0300

    adiciona obriga pelo menos 1 produto

--- src/Carrinho.ts
+++ src/Carrinho.ts
@@ -3,13 +3,13 @@ import { Produto } from './Produto'
 export class Carrinho {
   private readonly produtos: Produto[] = []
 
-  adiciona(...produtos: Produto[]): void {
-    this.produtos.push(...produtos)
+  adiciona(produto: Produto, ...produtos: Produto[]): void {
+    this.produtos.push(produto, ...produtos)
   }
 
   toString(): string {
     return 'Conteúdo do Carrinho:\n' +
       '=====================\n' +
-      this.produtos.join('\n') // junta os produtos separados por nova linha
+      this.produtos.join('\n')
   }
 }
